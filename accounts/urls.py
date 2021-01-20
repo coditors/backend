3e934e925e6193f3e47abac9e4fef27072e6f34e
Mazdak Pakaghideh <babamazdak@gmail.com>
2021-01-20 19:37:56 +0330

    add login and register api

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,14 @@
 from django.contrib import admin
 from django.urls import path , include
 from . import views
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
   
     path('', include('django.contrib.auth.urls')),
-    path('register/' , views.register , name="register")
+    path('register/' , views.register , name="register"),
+    path('v1/token/' , jwt_views.TokenObtainPairView.as_view() , name="token_obtain_pair"),
+    path("v1/refresh/" ,jwt_views.TokenRefreshView.as_view() , name="token_refresh"),
+    path('v1/register/',  include('rest_auth.registration.urls')),
+
 ]
