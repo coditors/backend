ce34a9b48f746d4ceb47d416e516789e94d0d52b
mohadeseh-sharifi <73468861+mohadeseh-sharifi@users.noreply.github.com>
2021-03-18 13:11:02 +0330

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -125,20 +125,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'coditors02',
-        'USER': 'root',
-        'PASSWORD': '1382',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
